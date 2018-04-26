3ca75770a3856ff53affc9f1958a4a9915089528
Sergey Savtyra <pandinator@protonmail.com>
2018-04-26 11:58:45 +0300

    Responsive markup of the  whole footer and MVP of its possibilities

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,15 +3,203 @@ import React, { Component } from 'react'
 import './style.css'
 
 export default class Footer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      chooseFilter: false,
+      creatingFilter: false,
+      newFilter: {},
+      currentFilter: {
+        id: 0,
+        name: 'none',
+        color: ''
+      },
+      colorInputNotSupported: false,
+      filters: [
+        {
+          id: 0,
+          name: 'none',
+          color: ''
+        },
+        {
+          id: 1,
+          name: 'Intelligence',
+          color: '#FFADC6'
+        },
+      ],
+      colors: [
+        {
+          id: 0,
+          value: '#D8D8D8'
+        },
+        {
+          id: 1,
+          value: '#b97375'
+        },
+        {
+          id: 2,
+          value: '#00FFCD'
+        },
+        {
+          id: 3,
+          value: '#E3D3E4'
+        },
+        {
+          id: 4,
+          value: '#A1CDF4'
+        },
+        {
+          id: 5,
+          value: '#9d8df1'
+        },
+        {
+          id: 6,
+          value: '#48A9A6'
+        },
+        {
+          id: 7,
+          value: '#D4B483'
+        },
+        {
+          id: 8,
+          value: '#FFADC6'
+        },
+      ],
+    }
+  }
+
+  toggleFilterChoose = () => {
+    const { chooseFilter } = this.state
+    this.setState({ chooseFilter: !chooseFilter })
+  }
+
+  toggleFilterCreating = () => {
+    const { creatingFilter } = this.state
+    this.setState({ creatingFilter: !creatingFilter })
+  }
+  
+  selectFilter = filter => {
+    this.setState({ chooseFilter: false, currentFilter: filter })
+  }
+
+  changeNewFilterInfo = (info, changingColor = false) => {
+    const { newFilter } = this.state
+    if (changingColor) {
+      this.setState({ newFilter: { ...newFilter, color: info } })
+      return
+    }
+    const { value, name } = info.target
+    this.setState({ newFilter: { ...newFilter, [name]: value } })
+  }
+
+  createFilter = () => {
+    const { newFilter, filters } = this.state
+    this.setState({ 
+      newFilter: { id: filters.length + 1, name: '', color: '#D8D8D8' }, 
+      filters: [...filters, newFilter],
+      creatingFilter: false
+    })
+  }
+
+  componentDidMount = () => {
+    const { filters } = this.state
+    this.setState({ newFilter: { id: filters.length, name: '', color: '#D8D8D8' } })
+  }
+
+  componentWillMount = () => {
+    // Checking if input[type="color"] is supported by a user`s browser
+    let input = document.createElement("input")
+    input.type = "color"
+    if (input.type !== "color") {
+      this.setState({ colorInputNotSupported: true })
+    }
+  }
+  
+
   render() {
+    const { 
+      chooseFilter, creatingFilter, currentFilter, newFilter, 
+      filters, colors, colorInputNotSupported,
+    } = this.state
+    const reversedFilters = filters.slice(0).reverse()
     return (
       <footer className="fixed">
         <div className="wrapper">
-            <div className="filters">
-                Filter: 
-                <span className="filter none">none</span>
-            </div>
-            <i className="material-icons">add_circle</i>
+            {
+              !creatingFilter ? (
+                <div className="filters">
+                  Filter:
+                  {
+                    chooseFilter && reversedFilters.map(filter => filter.id === 0 ? (
+                      <span className="filter none" onClick={() => this.selectFilter(filter)}>
+                        none
+                      </span>
+                    ) : (
+                      <div 
+                        key={filter.id} 
+                        className="filter" 
+                        style={{ background: filter.color }}
+                        onClick={() => this.selectFilter(filter)}
+                      >
+                        {filter.name}
+                      </div>
+                    ))
+                  }
+                  {
+                    !chooseFilter ?
+                      currentFilter.id === 0 ? (
+                        <span className="filter none active" onClick={this.toggleFilterChoose}>
+                          none
+                        </span>
+                      ) : (
+                        <div className="filter" onClick={this.toggleFilterChoose} style={{ background: currentFilter.color }}>
+                          {currentFilter.name}
+                        </div>
+                      )
+                    : ''
+                  }
+                </div>
+              ) : (
+                <form className="new-filter" onSubmit={e => {
+                  e.preventDefault()
+                  this.createFilter()
+                }}>
+                  <div className="left">
+                    <i className="material-icons" onClick={this.toggleFilterCreating}>close</i>
+                    <input className="line-based" placeholder="Filter name" name="name" onChange={this.changeNewFilterInfo} required/>
+                  </div>
+                  <div className="right">
+                    <div className="colors">
+                      {
+                        colors.map(color => (
+                          <div 
+                            key={color.id} 
+                            className={'color ' + (newFilter.color === color.value ? 'selected' : '')}
+                            style={{ background: color.value }}
+                            onClick={e => this.changeNewFilterInfo(color.value, true)}
+                          >
+                          </div>
+                        ))
+                      }
+                      {
+                        !colorInputNotSupported && (
+                          <div className="color-input">
+                            <input type="color" name="color" onChange={this.changeNewFilterInfo} />
+                            <i className="material-icons">change_history</i>
+                          </div>
+                        )
+                      }
+                    </div>
+                    <button type="submit">
+                      <i className="material-icons">done</i>
+                    </button>
+                  </div>
+                </form>
+              )
+            }
+            {
+              !creatingFilter && <i onClick={this.toggleFilterCreating} className="material-icons">add_circle</i>
+            }
         </div>
       </footer>
     )
